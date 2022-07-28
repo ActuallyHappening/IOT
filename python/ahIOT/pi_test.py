a4f6a7937fc327dfd37dd4b3ce940c45c4654899
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-07-28 19:51:15 +1000

    Added stream test

--- python/ahIOT/pi_test.py
+++ python/ahIOT/pi_test.py
@@ -1,4 +1,5 @@
 from functools import cache
+import uuid
 
 
 @cache
@@ -19,5 +20,23 @@ def test_aio_lib_import():
   if AIO.aio is None:
     raise NotImplementedError("Please instinate AIO.aio with credentials (.env file)")
   assert AIO.aio.send_stream_data
+  assert AIO.aio.receive_stream_data
   return AIO.aio
 
+def check_AIO(post, receive, extractID=lambda s: s.split(":")[1]):
+  id = str(uuid.uuid4())
+  
+  post(id)
+  received = receive()
+  
+  assert received
+  receivedID = extractID(received)
+  assert receivedID == id
+
+def test_aio_stream_works():
+  aio = test_aio_lib_import()
+  check_AIO(
+    lambda id: aio.send_stream_data(f"Send from pytest (__stream_data__) test_aio_stream_works:{id}"),
+    lambda: aio.receive_stream_data(),
+  )
+  return aio
