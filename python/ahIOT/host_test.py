5b08f77bc84edbb31ae0cb256133de0cf67efc6b
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-07-28 20:41:00 +1000

    Adding host.py tests ...

---
+++ python/ahIOT/host_test.py
@@ -0,0 +1,13 @@
+from functools import cache
+from .do_test import test_host_import
+host = test_host_import()
+
+@cache
+def test_host_main_exists():
+  assert host.main
+  return host.main
+
+@cache
+def test_host_step_exists():
+  assert host.step
+  return host.step
