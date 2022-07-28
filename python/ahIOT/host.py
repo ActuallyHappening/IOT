0595d55b6f749fb29311ec2fa48e2ee7aa7c8f60
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-07-28 21:23:00 +1000

    OK host looks good ...

--- python/ahIOT/host.py
+++ python/ahIOT/host.py
@@ -3,15 +3,18 @@ from .lib.AIO import aio
 from .lib.ThermalSensor.Extract_raw import iterate, print_frame
 
 def _load():
+  data = False
   try:
     loaded = aio.receive_stream_data()
     data = json.loads(loaded)
   except json.JSONDecodeError as exc:
     print(f"(Error: Received stream bad: {exc}")
-    aio.pi_status_send_code("JSONDecodeError - host.py")
+    print(f"{loaded=}")
+    aio.host_status_send("JSONDecodeError - host.py")
+  return data
 
 def _print(stream):
-  iterate(print_frame, stream)
+  iterate(print_frame, frame=stream)
 
 def _ping(*x, **y):
   aio.host_ping(*x, **y)
@@ -19,6 +22,9 @@ def _ping(*x, **y):
 def step():
   _ping()
   data = _load()
+  if data is False:
+    print("(Error: No data received)")
+    return
   _print(data)
 
 def main():
