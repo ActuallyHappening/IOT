694aeaae22031ac4072ed43c6fd200274a7371f2
Actually Happening - AH (from VM) <105958073+ActuallyHappening@users.noreply.github.com>
2022-07-30 21:18:06 +1000

    Trying to get dynamic colours working!

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -8,12 +8,13 @@ const _defaultFrame: number[][] = []
 for (let y = 0; y < defaultDimensions[1]; y++) {
   _defaultFrame[y] = []
   for (let x = 0; x < defaultDimensions[0]; x++) {
-    _defaultFrame[y][x] = [0, "red"]
+    _defaultFrame[y][x] = 0
   }
 }
 
 function App() {
   const [frame, setFrame] = useState(_defaultFrame)
+  console.log(frame)
   const frameTotal = frame.reduce((acc, row) => {
     return acc += row.reduce((acc, cell) => acc += cell, 0)
   }, 0)
@@ -29,9 +30,12 @@ function App() {
 
   return (
     <>
+    {/* <h2>oops</h2> */}
+    <Cell pos={[0, 0]} colour={getColour(frame[0][0])} />
     {frame.map((row, y) => {
       row.map((cell, x) => {
-        return <Cell pos={[x, y]} colour={getColour(cell)} />
+        // return <Cell pos={[x, y]} colour={getColour(cell)} />
+        return <h1>y</h1>
       })
     })}
     </>
--- react/src/components/Cell.tsx
+++ react/src/components/Cell.tsx
@@ -1,19 +1,24 @@
 import React, { CSSProperties } from 'react'
 
-const Cell = ({pos, colour}: {
+const Cell = ({
+  pos,
+  colour = "black",
+  scale = 25,
+}: {
   pos: [number, number],
-  colour: CSSProperties['color']
+  colour: CSSProperties['color'],
+  scale?: number,
 }) => {
   return (
     <div style={{
       position: 'absolute',
-      top: pos[0] * 10,
-      left: pos[1] * 10,
-      width: 10,
-      height: 10,
+      top: pos[0] * scale,
+      left: pos[1] * scale,
+      width: scale,
+      height: scale,
       backgroundColor: colour,
       border: '1px solid black'
-    }}>X</div>
+    }}>T</div>
   )
 }
 
