19b1922a922905e2ce59735e3f848d57d475f113
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-08-01 19:23:16 +1000

    Refactored AIO code into AIO.dart successfully

--- flutter/thermal_camera/lib/main.dart
+++ flutter/thermal_camera/lib/main.dart
@@ -6,76 +6,7 @@ import 'package:http/http.dart' as http;
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-Future<ThermalStream> fetchStream() async {
-  final String username = dotenv.env["ADAFRUIT_IO_USERNAME"] as String;
-  final String key = dotenv.env["ADAFRUIT_IO_KEY"] as String;
-  final response = await http.get(
-      Uri.parse(
-          'https://io.adafruit.com/api/v2/$username/feeds/brad.ir-stream/data?limit=1'),
-      headers: {'X-AIO-KEY': key});
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    final streamData = json.decode(response.body)[0];
-    return ThermalStream.fromJson(streamData);
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
-
-class ThermalStream {
-  final List<int> stream;
-
-  static const defaultDimensions = [
-    24,
-    32,
-  ];
-
-  const ThermalStream({required this.stream});
-
-  static Future<ThermalStream> fetch() async {
-    return await fetchStream();
-  }
-
-  static List<List<int>> parse({required List<int> stream}) {
-    const dimensions = defaultDimensions;
-    final width = dimensions[0];
-    final height = dimensions[1];
-    final pixels = List<List<int>>.generate(height, (y) {
-      return List<int>.generate(width, (x) {
-        final index = y * width + x;
-        return stream[index];
-      });
-    });
-    return pixels;
-  }
-
-  factory ThermalStream.fromJson(Map<String, dynamic> json) {
-    final value = jsonDecode(json['value']);
-    final rawStream = value['stream'];
-    final List<dynamic> parsedStream;
-    try {
-      parsedStream = rawStream as List<dynamic>;
-    } on FormatException {
-      throw Exception(
-          'Invalid stream data (probably cam not plugged in), got $rawStream');
-    } catch (e) {
-      throw Exception('Invalid stream data (probably corrupted data), e=$e');
-    }
-    // parsedStream = "[$rawStream]";
-    final List<int> finishedStream;
-    try {
-      finishedStream = parsedStream.map((e) => e as int).toList();
-    } catch (e) {
-      throw Exception(
-          'Invalid stream data (probably corrupted or NaN data)\n Raw error = $e');
-    }
-    return ThermalStream(stream: finishedStream);
-  }
-}
+import 'AIO.dart' as AIO;
 
 Future main() async {
   await dotenv.load(fileName: ".env");
@@ -90,12 +21,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late Future<ThermalStream> currentStream;
+  late Future<AIO.ThermalStream> currentStream;
 
   @override
   void initState() {
     super.initState();
-    currentStream = fetchStream();
+    currentStream = AIO.fetchStream();
   }
 
   @override
@@ -110,7 +41,7 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Fetch Data Example'),
         ),
         body: Center(
-          child: FutureBuilder<ThermalStream>(
+          child: FutureBuilder<AIO.ThermalStream>(
             future: currentStream,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
