dfa6c2bc07bbc61e62b674df4b1a2542077bcd4f
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-08-03 19:44:55 +1000

    refactoring ble library

--- micropython/iot v2/lib/ble/ble.py
+++ micropython/iot v2/lib/ble/ble.py
@@ -1,13 +1,7 @@
-from .command import Command, _timeoutWrapper
-from Bluetooth import ble_uart_peripheral as ble_DO
-
-
-def _bluetoothCallback(msg): return print(
-    f"$< BluetoothCommands received: {msg}")
-
+from blelib import ble_uart_peripheral as ble_DO
 
 __uart = None
-
+_uart_buffer = "";
 
 def dualUartPrint(msg, *, defaultPrint=print, shouldDefaultPrint=True, __prefix__=""):
     if shouldDefaultPrint:
@@ -15,11 +9,23 @@ def dualUartPrint(msg, *, defaultPrint=print, shouldDefaultPrint=True, __prefix_
     if __uart:
         __uart.write(f"{__prefix__}{msg}\n")
 
+async def __execute__(command: str, *, logger=dualUartPrint, **kwargs):
+    logger(f"$> BluetoothCommands execute: {command}")
+    parsed = command.lower().split(" ")
+    if parsed[0] == "send":
+      await send(command, logger=logger, **kwargs)
+    elif parsed[0] == "receive":
+      return _uart_buffer
+    elif parsed[0] == "begin":
+      await begin(logger=logger, **kwargs)
+    logger(f"$< BluetoothCommands execute: {command}")
+
+def _bluetoothCallback(msg): return print(
+    f"$< BluetoothCommands received: {msg}")
 
 __implicitRetry = False
 
 
-@_timeoutWrapper
 async def send(msg=..., *, __constructor__=..., logger=dualUartPrint, **kwargs):
     global __implicitRetry
     if msg is ...:
@@ -40,8 +46,6 @@ async def send(msg=..., *, __constructor__=..., logger=dualUartPrint, **kwargs):
         __implicitRetry = True
         await send(msg, logger=logger, **kwargs)
 
-
-@_timeoutWrapper
 async def begin(*, logger=dualUartPrint, handler=_bluetoothCallback, **kwargs):
     global __uart
     logger("$> BluetoothCommands Beginning ...")
@@ -54,7 +58,9 @@ async def begin(*, logger=dualUartPrint, handler=_bluetoothCallback, **kwargs):
 
 
 def ThetaHandler(msg, logger=dualUartPrint, **kwargs):
+    global _uart_buffer
     logger(f"$< BluetoothCommands received: {msg}")
+    _uart_buffer = msg
 
 
 async def ProjectTheta(*, logger=dualUartPrint, handler=..., __constructor__=..., **kwargs):
@@ -72,9 +78,3 @@ async def ProjectTheta(*, logger=dualUartPrint, handler=..., __constructor__=...
     await send("PROJECT THETA is a go :)", logger=logger, **kwargs)
 
     logger("$> Finished BluetoothCommands.py (Project Theta) bluetooth")
-
-commands = {
-    "Bluetooth Begin": Command(begin),
-    "Bluetooth Send": Command(send),
-    "Bluetooth Project Theta": Command(ProjectTheta),
-}
