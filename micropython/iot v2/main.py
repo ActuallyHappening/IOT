8269a10faad1a9a254c308a06248c7a2a2037c00
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-08-05 09:51:34 +1000

    main now starts bletooth

--- micropython/iot v2/main.py
+++ micropython/iot v2/main.py
@@ -4,9 +4,12 @@ try:
 except ImportError:
   import asyncio as asio # type: ignore
 
+from lib.ble import begin
+
 def main():
   do("forall motor stop")
   do("ble begin")
+  begin(handler=do)
   
 if __name__ == "__main__":
   main()
