9c0f8f5b8f97bbe038c0131b27bbe727101b6ba4
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-08-04 19:33:52 +1000

    cmd now hot-imports when needed

--- micropython/iot v2/cmd.py
+++ micropython/iot v2/cmd.py
@@ -1,22 +1,21 @@
-from .lib.aio import __execute__ as aio
-from .lib.ble import __execute__ as ble
-from .lib.gpio import __execute__ as gpio
-from .lib.wifi import __execute__ as wifi
-from .lib.gpio.hbridge import __execute__ as motor
-
 async def do(cmd: str):
     parsed = cmd.lower().split(" ")
     tlc = parsed[0]
     _cmd = cmd[1:]
     if tlc == "ble":
+        from lib.ble import __execute__ as ble
         await ble(_cmd)
     elif tlc == "aio":
+        from lib.aio import __execute__ as aio
         await aio(_cmd)
     elif tlc == "wifi":
+        from lib.wifi import __execute__ as wifi
         await wifi(_cmd)
     elif tlc == "gpio":
+        from lib.gpio import __execute__ as gpio
         await gpio(_cmd)
     elif tlc == "motor":
+        from lib.gpio.hbridge import __execute__ as motor
         await motor(_cmd)
     else:
         raise ValueError(f"Invalid tlc for cmd: {cmd}")
