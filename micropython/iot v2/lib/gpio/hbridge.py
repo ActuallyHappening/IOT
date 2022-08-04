82417586f19f470ac6e48194b27ea5daf4444024
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-08-04 19:52:25 +1000

    hbridge typing now no typing imports

--- micropython/iot v2/lib/gpio/hbridge.py
+++ micropython/iot v2/lib/gpio/hbridge.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+# from typing import Tuple # micropython doesn't support typing
 import machine
 import uasyncio as asio
 import gpio
@@ -12,7 +12,7 @@ async def _do(tlc: str, param1: int = None, param2: int = None) -> None:
   
     # If param2 not exits, then motor uses param1 as motor num
     # If param2 exists, then motor is (param1, param2)
-    _motor: Tuple[int, int]
+    _motor = None # type Tuple[int, int]| None
     if param1:
       if param2: _motor = (param1, param2)
       else:
@@ -58,19 +58,19 @@ async def __execute__(cmd: str):
             raise ValueError(f"Invalid pin numbers for cmd: {cmd}") from exc
         await _do(tlc, pin1Num, pin2Num)
 
-async def forward(motor: Tuple[int, int]) -> None:
+async def forward(motor: "Tuple[int, int]") -> None:
     await gpio_do(f"on {motor[0]}")
     await gpio_do(f"off {motor[1]}")
 
-async def backward(motor: Tuple[int, int]) -> None:
+async def backward(motor: "Tuple[int, int]") -> None:
     await gpio_do(f"off {motor[0]}")
     await gpio_do(f"on {motor[1]}")
 
-async def stop(motor: Tuple[int, int]) -> None:
+async def stop(motor: "Tuple[int, int]") -> None:
     await gpio_do(f"off {motor[0]}")
     await gpio_do(f"off {motor[1]}")
 
-async def step(motor: Tuple[int, int], time: int = 1) -> None:
+async def step(motor: "Tuple[int, int]", time: int = 1) -> None:
     await forward(motor)
     await asio.sleep(time)
     await stop(motor)
