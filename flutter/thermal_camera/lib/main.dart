b970d52f313d7e9b54e0e8793c0804f784d1ce19
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-08-13 20:43:54 +1000

    Got it to work with entitlements and stuff :)

--- flutter/thermal_camera/lib/main.dart
+++ flutter/thermal_camera/lib/main.dart
@@ -1,27 +1,26 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 void main() {
-  debugPrint("Running!");
-  debugPaintSizeEnabled = true;
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
+
+  static const _title = "Flutter layout tutorial";
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-        title: 'Learning',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Learning'),
-          ),
-          body: const Center(
-            child: Text('Hello World'),
-          ),
-        ));
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(_title),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
   }
 }
