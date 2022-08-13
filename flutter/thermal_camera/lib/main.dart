e44a5b984468412b9b24cf54e7a012037f015cf9
Actually Happening - AH (from VM) <105958073+ActuallyHappening@users.noreply.github.com>
2022-08-13 20:02:16 +1000

    Learning flutter!

--- flutter/thermal_camera/lib/main.dart
+++ flutter/thermal_camera/lib/main.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   debugPrint("Running!");
-  runApp(
-    const Center(
-      child: Text("Hello, world!"),
-    ),
-  );
+  debugPaintSizeEnabled = true;
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+        title: 'Learning',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Learning'),
+          ),
+          body: const Center(
+            child: Text('Hello World'),
+          ),
+        ));
+  }
 }
