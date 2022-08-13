dd098b65c5e354d7b2028eddfed91fb11e7e377e
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-08-13 21:12:06 +1000

    Got it to work with entitlements and stuff :)

--- flutter/thermal_camera/lib/main.dart
+++ flutter/thermal_camera/lib/main.dart
@@ -1,27 +1,37 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 void main() {
-  debugPrint("Running!");
-  debugPaintSizeEnabled = true;
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
+
+  static const _title = "Flutter layout tutorial";
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-        title: 'Learning',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Learning'),
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(_title),
+        ),
+        body: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          const Center(
+          child: Text('Hello World'),
           ),
-          body: const Center(
-            child: Text('Hello World'),
+          const Center(child: Text("Yay!")),
+          CachedNetworkImage(
+            imageUrl: "http://via.placeholder.com/350x150",
+            progressIndicatorBuilder: (context, url, downloadProgress) =>
+                CircularProgressIndicator(value: downloadProgress.progress),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
           ),
-        ));
+          Icon(Icons.alarm, color: Colors.red[600])
+        ]),
+      ),
+    );
   }
 }
