4777eb8b10dcbc48dd5b339d424d39c659a27880
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-08-16 20:04:42 +1000

    Why is my touchpad controlling another computer?

--- flutter/ah/lib/main.dart
+++ flutter/ah/lib/main.dart
@@ -1,21 +1,359 @@
-import 'package:ah/routing.dart';
 import 'package:flutter/material.dart';
+import 'package:touch_bar/touch_bar.dart';
 
-void main() async {
-  debugPrint("Starting ...");
-  runApp(const MyApp());
+void main() {
+  print("Running cool touchbar example");
+
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  MaterialColor themeColor = Colors.blue;
+
+  void _changeThemeColor(MaterialColor color) {
+    setState(() {
+      themeColor = color;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      initialRoute: MyRouting.initialRoute,
-      routes: MyRouting.routes,
+      theme: ThemeData(
+        primarySwatch: themeColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(
+        title: 'Flutter Demo Home Page',
+        changeThemeColor: _changeThemeColor,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  final Function changeThemeColor;
+  const MyHomePage({
+    Key? key,
+    required this.title,
+    required this.changeThemeColor,
+  }) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static List<Color> colors = [
+    Colors.blue,
+    Colors.red,
+    Colors.green,
+    Colors.yellow,
+    Colors.pink,
+    Colors.purple,
+    Colors.grey,
+    Colors.orange,
+    Colors.brown,
+    Colors.white,
+    Colors.redAccent,
+    Colors.lime,
+  ];
+  Color buttonColor = Colors.blue;
+  TouchBarPopover popover = TouchBarPopover(
+    label: 'Options',
+    iconPosition: ImagePosition.left,
+    children: [
+      TouchBarLabel('Yellow', textColor: Colors.yellow),
+      TouchBarSpace.small(),
+      TouchBarLabel('Pink', textColor: Colors.pink),
+      TouchBarSpace.large(),
+      TouchBarLabel('Red', textColor: Colors.red),
+      TouchBarSpace.flexible(),
+      TouchBarLabel('Blue', textColor: Colors.blue),
+    ],
+    showCloseButton: true,
+  );
+  int _counter = 0;
+  late TouchBar bar;
+  TouchBarImage? plusImage;
+  TouchBarImage? minusImage;
+  TouchBarImage? menuImage;
+  TouchBarImage? menu2Image;
+  TouchBarLabel popoverLabel1 = TouchBarLabel('Popover 1');
+  TouchBarLabel popoverLabel2 = TouchBarLabel('Popover 2');
+  late TouchBarScrubber scrubber;
+  List<TouchBarScrubberItem> scrubberChildren = [];
+  bool isIncrementing = true;
+  bool isHighlightingTheColor = false;
+  Color highlightColor = Colors.blue;
+
+  TouchBarButton actionButton = TouchBarButton(
+    label: "Increment 1",
+    backgroundColor: Colors.green,
+    iconPosition: ImagePosition.right,
+  );
+
+  TouchBarLabel counterLabel = TouchBarLabel(
+    'Counter: 0',
+    textColor: Colors.red,
+  );
+  TouchBarLabel timesTwoCounterLabel = TouchBarLabel(
+    'Counter: 2',
+    textColor: Colors.blue,
+  );
+
+  void _invertOperatorIfNeeded() {
+    Function()? onClick;
+    String label;
+    String popoverLabel;
+    Color color;
+    ImagePosition position;
+    bool popoverShowCloseButton;
+    TouchBarImage? operatorImage;
+    TouchBarImage? popoverImage;
+    List<TouchBarItem> popoverChildren;
+
+    bool scrubberShowArrowButtons;
+    bool scrubberIsContinuos;
+    ScrubberMode scrubberMode;
+    ScrubberSelectionStyle scrubberOverlayStyle;
+    ScrubberSelectionStyle scrubberSelectedStyle;
+    bool isIncrementing;
+
+    if (_counter >= 10) {
+      isIncrementing = false;
+      popoverShowCloseButton = false;
+      onClick = _decrementCounter;
+      label = 'Decrement 1';
+      color = Colors.red;
+      position = ImagePosition.left;
+      popoverLabel = 'Menu 2';
+      operatorImage = minusImage;
+      popoverImage = menu2Image;
+      popoverChildren = [timesTwoCounterLabel, popoverLabel1, actionButton];
+
+      scrubberShowArrowButtons = false;
+      scrubberIsContinuos = false;
+      scrubberMode = ScrubberMode.free;
+      scrubberOverlayStyle = ScrubberSelectionStyle.none;
+      scrubberSelectedStyle = ScrubberSelectionStyle.outlineOverlay;
+    } else if (_counter <= 0) {
+      isIncrementing = true;
+      popoverShowCloseButton = true;
+      onClick = _incrementCounter;
+      label = 'Increment 1';
+      color = Colors.green;
+      position = ImagePosition.right;
+      popoverLabel = 'Menu 1';
+      operatorImage = plusImage;
+      popoverImage = menuImage;
+      popoverChildren = [actionButton, popoverLabel2, timesTwoCounterLabel];
+
+      scrubberShowArrowButtons = true;
+      scrubberIsContinuos = true;
+      scrubberMode = ScrubberMode.fixed;
+      scrubberOverlayStyle = ScrubberSelectionStyle.outlineOverlay;
+      scrubberSelectedStyle = ScrubberSelectionStyle.roundedBackground;
+    } else {
+      return; // do not invert operator
+    }
+
+    setState(() {
+      actionButton.onClick = onClick;
+      actionButton.label = label;
+      actionButton.backgroundColor = color;
+      actionButton.iconPosition = position;
+      actionButton.icon = operatorImage;
+
+      popover.iconPosition = position;
+      popover.label = popoverLabel;
+      popover.showCloseButton = popoverShowCloseButton;
+      popover.icon = popoverImage;
+      popover.children = popoverChildren;
+
+      scrubber.showArrowButtons = scrubberShowArrowButtons;
+      scrubber.isContinuos = scrubberIsContinuos;
+      scrubber.mode = scrubberMode;
+      scrubber.overlayStyle = scrubberOverlayStyle;
+      scrubber.selectedStyle = scrubberSelectedStyle;
+
+      this.isIncrementing = isIncrementing;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+      counterLabel.label = 'Counter: $_counter';
+      counterLabel.textColor = colors[_counter % colors.length];
+      timesTwoCounterLabel.label = 'Counter: ${_counter * 2}';
+      timesTwoCounterLabel.textColor = colors[(_counter + 1) % colors.length];
+
+      scrubberChildren.add(TouchBarScrubberLabel(
+        "Another Color",
+        textColor: colors[_counter % colors.length],
+      ));
+      scrubber.children = scrubberChildren;
+    });
+
+    _invertOperatorIfNeeded();
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      counterLabel.label = 'Counter: $_counter';
+      counterLabel.textColor = colors[_counter % colors.length];
+      timesTwoCounterLabel.label = 'Counter: ${_counter * 2}';
+      timesTwoCounterLabel.textColor = colors[(_counter + 1) % colors.length];
+
+      scrubberChildren.removeLast();
+      scrubber.children = scrubberChildren;
+    });
+
+    _invertOperatorIfNeeded();
+  }
+
+  void _onClick() {
+    if (isIncrementing) {
+      _incrementCounter();
+    } else {
+      _decrementCounter();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    try {
+      setTouchBar(TouchBar(
+          children: [TouchBarLabel('Testing if touch bar exists ...')]));
+    } catch (e) {
+      print("YAY the touch bar not-working has been detected! $e");
+    }
+    // loadAsyncData();
+  }
+
+  void loadAsyncData() async {
+    final plusImage = await TouchBarImage.loadFrom(
+      path: 'icons/test.png',
+    );
+    final minusImage = await TouchBarImage.loadFrom(
+      path: 'icons/test.png',
+    );
+    final menuImage = await TouchBarImage.loadFrom(
+      path: 'icons/test.png',
+    );
+    final menu2Image = await TouchBarImage.loadFrom(
+      path: 'icons/test.png',
+    );
+    scrubberChildren = [
+      ...colors
+          .asMap()
+          .map((idx, color) => MapEntry(
+              idx,
+              TouchBarScrubberLabel(
+                'Another Color $idx',
+                textColor: color,
+              )))
+          .values
+          .toList(),
+      TouchBarScrubberImage(plusImage),
+    ];
+    scrubber = TouchBarScrubber(
+        children: scrubberChildren,
+        selectedStyle: ScrubberSelectionStyle.roundedBackground,
+        overlayStyle: ScrubberSelectionStyle.outlineOverlay,
+        mode: ScrubberMode.fixed,
+        onSelect: (childId) {
+          setState(() => isHighlightingTheColor = false);
+          widget.changeThemeColor(
+            colors[childId % colors.length],
+          );
+        },
+        onHighlight: (childId) => setState(() {
+              isHighlightingTheColor = true;
+              highlightColor = colors[childId % colors.length];
+            }));
+
+    actionButton.icon = plusImage;
+    actionButton.onClick = _incrementCounter;
+
+    popover.icon = menuImage;
+
+    bar = TouchBar(children: [
+      actionButton,
+      TouchBarLabel('Red Label', textColor: Colors.red),
+      TouchBarLabel('Blue Label', textColor: Colors.blue),
+      counterLabel,
+      popover,
+      TouchBarPopover(
+        label: 'Colors',
+        children: [scrubber],
+        showCloseButton: true,
+      )
+    ]);
+    setTouchBar(bar);
+
+    setState(() {
+      bar = bar;
+      this.plusImage = plusImage;
+      this.minusImage = minusImage;
+      this.menuImage = menuImage;
+      this.menu2Image = menu2Image;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (isHighlightingTheColor)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text('You are highlightining the color '),
+                  SizedBox(
+                    width: 20,
+                    height: 20,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: highlightColor,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _onClick,
+        tooltip: 'Increment/Decrement',
+        backgroundColor: buttonColor,
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
