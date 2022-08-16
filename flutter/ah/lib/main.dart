f5192c1ac1af0ef6c53f9dc295529406d0c212e4
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-08-16 19:59:52 +1000

    _

--- flutter/ah/lib/main.dart
+++ flutter/ah/lib/main.dart
@@ -1,41 +1,21 @@
-// Minimal reproducible code
-
+import 'package:ah/routing.dart';
 import 'package:flutter/material.dart';
-import 'package:touch_bar/touch_bar.dart';
 
-void main() {
+void main() async {
+  debugPrint("Starting ...");
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Future<void> doTouchBar() async {
-    try {
-      await setTouchBar(
-          TouchBar(children: [TouchBarLabel('Testing Touch Bar ...')]));
-    } catch (e) {
-      print(
-          "How do I catch this error when on a platform not supporting touch bar? $e");
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    doTouchBar();
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Touch Bar Demo',
-      home: Center(child: Text('Hello World!')),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: MyRouting.initialRoute,
+      routes: MyRouting.routes,
     );
   }
 }
