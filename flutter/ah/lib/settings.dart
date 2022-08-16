ef42531434b814f45c5f1be660747649196d481f
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-08-17 08:38:41 +1000

    New simple flutter project

--- flutter/ah/lib/settings.dart
+++ flutter/ah/lib/settings.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:menubar/menubar.dart';
-import 'package:touch_bar/touch_bar.dart';
 
 class SettingsRoute extends StatefulWidget {
   const SettingsRoute({Key? key}) : super(key: key);
@@ -10,44 +8,11 @@ class SettingsRoute extends StatefulWidget {
 }
 
 class _SettingsRouteState extends State<SettingsRoute> {
-  Future<void> touchBarAsync() async {
-    final settingsIcon =
-        await TouchBarImage.loadFrom(path: 'icons/settings.png');
-
-    TouchBar bar = TouchBar(children: [
-      TouchBarLabel('hello TOUCH BAR YAY!'),
-      TouchBarButton(
-          icon: settingsIcon,
-          label: 'Settings',
-          onClick: () {
-            print("Settings clicked YAYAYAY!");
-            Navigator.pop(context);
-          })
-    ]);
-    setTouchBar(bar);
-  }
-
-  menuBarAsync() {
-    setApplicationMenu([
-      NativeSubmenu(label: 'TEST submenu', children: [
-        NativeMenuItem(
-            label: 'test item',
-            onSelected: () {
-              print('test item clicked');
-            }),
-        NativeMenuItem(
-            label: 'test item 2',
-            onSelected: () {
-              print('test item 2 clicked');
-            }),
-      ]),
-    ]);
-  }
 
   @override
   void initState() {
     super.initState();
-    touchBarAsync();
+    // TODO: implement initState
   }
 
   @override
---
+++ flutter/simnple/android/app/src/main/kotlin/com/example/simnple/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.simnple
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
