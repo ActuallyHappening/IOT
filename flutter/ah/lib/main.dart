91fa9a37b819e9d04eea0c9c085a96054c8b4bdf
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-08-21 14:02:52 +1000

    Initializing firebase ...

--- flutter/ah/lib/main.dart
+++ flutter/ah/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:ah/routing.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
 
 void main() async {
   debugPrint("Starting ...");
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatefulWidget {
