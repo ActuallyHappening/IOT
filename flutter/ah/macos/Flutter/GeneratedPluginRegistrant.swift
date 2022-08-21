158375c0c76e9cd9db7c6a8c1f3a943c433090b8
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-08-21 14:58:32 +1000

    Removed touch bar from pubspec

--- flutter/ah/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter/ah/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,8 @@ import Foundation
 
 import firebase_core
 import menubar
-import touch_bar_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   MenubarPlugin.register(with: registry.registrar(forPlugin: "MenubarPlugin"))
-  TouchBarPlugin.register(with: registry.registrar(forPlugin: "TouchBarPlugin"))
 }
