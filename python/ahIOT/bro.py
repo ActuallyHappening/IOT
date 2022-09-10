14fb24220d550f29b34baf5290553e7d2bae481a
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-09-10 18:07:55 +1000

    added bro.py

---
+++ python/ahIOT/bro.py
@@ -0,0 +1,26 @@
+import time
+import board
+import busio
+import adafruit_mlx90640
+from firebase import send_firebase
+
+i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
+
+mlx = adafruit_mlx90640.MLX90640(i2c)
+print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
+
+# if using higher refresh rates yields a 'too many retries' exception,
+# try decreasing this value to work with certain pi/camera combinations
+mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
+
+frame = [0] * 768
+while True:
+    try:
+        mlx.getFrame(frame)
+    except ValueError as exc:
+        # these happen, no biggie - retry
+        print(f"Failed frame {exc}")
+        continue
+    else:
+      send_firebase(frame)
+    
