e1ee0a6796d0c78c8731bd95e44f7f3814f85eb5
Actaully Happening - AH <105958073+ActuallyHappening@users.noreply.github.com>
2022-09-13 10:01:43 +1000

    sorted dart imports

--- flutter/ah_portal/lib/main.dart
+++ flutter/ah_portal/lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:ah_portal/views/default.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import 'package:ah_portal/views/default.dart';
 
 import 'firebase_options.dart';
 
